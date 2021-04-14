1be65b75ad9eb2ac7b7bca5d2944f894c70239d2
Alexander Lucano <sailex1030@gmail.com>
2021-04-13 23:11:52 -0500

    comentarios

--- 02-Project/2.py
+++ 02-Project/2.py
@@ -1,15 +1,18 @@
+# tenemos que instalar mysql connector
 import mysql.connector
-
+# esta coneccion esta hecha con una db del profesor del curso
 con = mysql.connector.connect(
     user="ardit700_student",
     password="ardit700_student",
     host="108.167.140.122",
     database="ardit700_pm1database"
 )
-
+# llamaos al cursor
 cursor = con.cursor()
 word = input("Enter a word: ")
+# hacemos un query a la base de datos
 query = cursor.execute(f"SELECT * FROM Dictionary Where Expression = '{word}'")
+# y aqui tenemos los resultados
 results = cursor.fetchall()
 
 if results:
